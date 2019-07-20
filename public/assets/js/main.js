1f6196ea44f4ae86de7019af07066c758a5c5181
fathi guemri <=>
2019-07-20 18:01:24 +0100

    add arabic lang and contanu design

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -18,7 +18,9 @@ $(function() {
     $('.open').on('click', function() {
         let urlVideo = $(this).attr('data-video');
         lecter.attr('src', urlVideo);
-        $(window).scrollTop(0)
+        $(window).scrollTop(0);
+        var data = JSON.parse($(this).attr('data-item'));
+        $('.v-info strong').text(data.title);
     });
 
     var close = setTimeout(function() {
@@ -29,4 +31,46 @@ $(function() {
 
     if (status == 'success') alert.addClass("alert-success");
     else alert.addClass("alert-danger");
+    $('.responsive').slick({
+        infinite: false,
+        speed: 1000,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+    $('.prev').on('click', () => {
+        $('.slick-prev').click();
+    });
+
+    $('.next').on('click', () => {
+        $('.slick-next').click();
+    });
 });
